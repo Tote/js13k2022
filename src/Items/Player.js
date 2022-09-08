de5e08b22de8fecf5b78f393bcde42ac5d1bb363
Patricio Morel <totenbot@outlook.com>
2022-09-08 01:14:39 -0300

    player variable MaxV

--- src/Items/Player.js
+++ src/Items/Player.js
@@ -3,16 +3,17 @@ import PhysicItem from "../../gloop/src/PhysicItem.js"
 export default class Player extends PhysicItem{
     constructor(){
         super()
+        this.maxV = 10
     }
 
     draw(g){
         g.color('limegreen').rect(this)
     }
 
-    goUp(){this.vy = -1}
-    goRight(){this.vx = 1}
-    goDown(){this.vy = 1}
-    goLeft(){this.vx = -1}
+    goUp(){this.vy = -this.maxV}
+    goRight(){this.vx = this.maxV}
+    goDown(){this.vy = this.maxV}
+    goLeft(){this.vx = -this.maxV}
     stopUp(){this.vy = 0}
     stopRight(){this.vx = 0}
     stopDown(){this.vy = 0}
